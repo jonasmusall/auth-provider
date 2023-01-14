1ca1ff4587d0f038ffec50f6f04241e5b00092ab
Jonas Musall <69427105+jonasmusall@users.noreply.github.com>
2023-01-14 03:42:29 +0100

    Add schemas and create first stub endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
-console.log('Hello world!');
+import fastify, { FastifyInstance } from 'fastify';
+import { IPostUser, postUserSchema } from './schemas.js';
+
+const PORT = 8888;
+
+const app: FastifyInstance = fastify();
+
+// TODO: RSA keypair
+// TODO: database
+
+app.post<{
+  Body: IPostUser
+}>(
+  '/user',
+  { schema: { body: postUserSchema } },
+  async (request, reply) => {
+    // TODO: check name form (RegEx?), check db if name exists, reply
+  }
+);
+
+app.listen({ port: PORT }, (err, addr) => {
+  if (err) throw err;
+  console.log(`Listening on ${addr}`);
+})
---
+++ src/schemas.ts
@@ -0,0 +1,47 @@
+export interface IPostUser {
+  name: string
+}
+
+export const postUserSchema = {
+  type: 'object',
+  properties: {
+    name: { type: 'string' }
+  }
+}
+
+export interface IPutFirstPassword {
+  firstPasswordToken: string,
+  password: string
+}
+
+export const putFirstPasswordSchema = {
+  type: 'object',
+  properties: {
+    firstPasswordToken: { type: 'string' },
+    password: { type: 'string' }
+  }
+}
+
+export interface IGetToken {
+  password: string
+}
+
+export const getTokenSchema = {
+  type: 'object',
+  properties: {
+    password: { type: 'string' }
+  }
+}
+
+export interface IPutPassword {
+  password: string,
+  newPassword: string
+}
+
+export const putPasswordSchema = {
+  type: 'object',
+  properties: {
+    password: { type: 'string' },
+    newPassword: { type: 'string' }
+  }
+}
